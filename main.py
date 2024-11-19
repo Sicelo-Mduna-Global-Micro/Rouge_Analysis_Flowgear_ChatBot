1e493f44fa95427933f8d9f84df26b916fd71ec1
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-11-20 00:20:14 +0200

    Single file text extractor

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from PyPDF2 import PdfReader
 
+def extract_text_from_pdf(file_path):
+    reader = PdfReader(file_path)
+    text = ''
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+# Example usage
+file_path = 'source_documents/source_sample_document_LL.pdf'
+extracted_text = extract_text_from_pdf(file_path)
+print(extracted_text)
